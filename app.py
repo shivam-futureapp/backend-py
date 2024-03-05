9bf438ef4451bd15e7f5b4bf5ed9f97d9e083c8f
shivam <shivam@shivam.com>
2024-03-05 20:06:50 +0530

    added a new message

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ CORS(backend_app)  # Enable CORS for all routes in the backend app
 
 @backend_app.route('/api/hello')
 def hello():
-    return jsonify({'message': 'Hello from the backend!'})
+    return jsonify({'message': 'Hello from the backend! everything works fine'})
 
 if __name__ == '__main__':
     backend_app.run(host='0.0.0.0', port=83)  # Run the backend app on port 5001
